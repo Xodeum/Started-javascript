203cfda23ab309bae831e0edb41b28bc35fbe141
Xodeum <99142808+Xodeum@users.noreply.github.com>
2024-07-08 16:36:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+    var a = "3";
+    var b = "8";
+    //write your code here:
+var c = a;
+    a = b;
+    b = c;
+
+    ////*****Don't change code below!!
+
+    console.log("a is " + a);
+    console.log("b is " + b);
